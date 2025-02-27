1b6d97c313457fad436f5c903519f46da49cb157
umesh raut <uraut6044@gmail.com>
2025-02-27 09:31:31 +0530

    Add JavaScript object for Tata Safari car with properties and methods

---
+++ week-5/ObjectCar.js
@@ -0,0 +1,13 @@
+let tataSafari = {
+    color: "Black",
+    brand: "Tata",
+    model: "Safari",
+    speciality: function () { 
+        return "Off-Road Driving";
+     },
+     price: 2000000,
+     isAvailable: true,
+     features: ["Power Stearing", "Power Windows", "Central Locking", "ABS", "Airbags"]
+}
+
+console.log(tataSafari);
