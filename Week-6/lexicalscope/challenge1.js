2b172c0af8c926a4aee1d1393116a238ea945739
umesh raut <uraut6044@gmail.com>
2025-03-01 20:52:03 +0530

    Add functions to demonstrate incrementing a count using lexical scope

---
+++ Week-6/lexicalscope/challenge1.js
@@ -0,0 +1,21 @@
+function increment(){
+    let count = 0;
+    return ount++;
+}
+
+function increment2(){
+    let count = 0;
+    return count++;
+}
+
+function increment3(){
+    let count = 0;
+    return count++;
+}
+
+
+console.log(increment());
+let count = 100;
+console.log(increment2());
+console.log(increment3());
+
