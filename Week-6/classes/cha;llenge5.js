f6c5d6ef327da02cac6a316c3f900f9522ee73da
umesh raut <uraut6044@gmail.com>
2025-03-03 19:00:41 +0530

    Add transfer method to BankAccount class for inter-account transfers

--- Week-6/classes/cha;llenge5.js
+++ Week-6/classes/cha;llenge5.js
@@ -15,6 +15,17 @@ BankAccount.prototype.withdraw = function(amount){
     return this.balance -= amount;
 }
 
+BankAccount.prototype.transfer = function(amount, targetAccount){
+    if(this.balance < amount){
+        return "Insufficient balance";
+    }
+    else{
+        this.balamce -= amount;
+        targetAccount.balance += amount;
+        return `Transfered: ${amount} to ${targetAccount.holderName}`;
+    }
+}
+
 const account1 = new BankAccount(123, 'John', 1000);
 const account2 = new BankAccount(124, 'Jane', 2000);
 
@@ -27,3 +38,8 @@ console.log(account1.balance);
 account1.withdraw(2000);
 
 console.log(account1.balance);
+
+console.log(account1.transfer(1000, account2));
+
+console.log(account1.balance);
+console.log(account2.balance);
