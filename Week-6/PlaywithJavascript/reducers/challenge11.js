0c402cec5836d9c5cf3334bc6e836e61f9806fe6
umesh raut <uraut6044@gmail.com>
2025-03-07 19:33:04 +0530

    challenge 11 completed

---
+++ Week-6/PlaywithJavascript/reducers/challenge11.js
@@ -0,0 +1,21 @@
+/*
+6️⃣ LinkedIn - User Endorsement Count for Skills
+
+📝 Problem:
+LinkedIn par ek user ke skills ka total endorsement count nikalna hai.
+*/
+
+// input: 
+
+const skills = [
+  { skill: "JavaScript", endorsements: 15 },
+  { skill: "React", endorsements: 10 },
+  { skill: "Node.js", endorsements: 8 }
+];
+
+const totalEndorsement = skills.reduce((acc, curr) => (acc + curr.endorsements),0);
+
+console.log("Total Endorsements: ",totalEndorsement);
+
+// output // Total Endorsements: 33
+
