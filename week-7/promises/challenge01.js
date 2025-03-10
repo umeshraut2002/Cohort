d15a5efa9d4cddaf26506ebdc24ffeb24cfd199b
umesh raut <uraut6044@gmail.com>
2025-03-10 23:58:22 +0530

    promise resolve

--- week-7/promises/challenge01.js
+++ week-7/promises/challenge01.js
@@ -3,3 +3,26 @@
 
 👉 Task: Ek function createPromise(flag) likh, jo agar flag === true ho toh "Success" return kare, warna "Error" return kare using .then/.catch.
 */
+
+const promise = new Promise((resolve, reject) => {
+  let flag = true;
+
+  if (flag) {
+    resolve("Sucess");
+  } else {
+    reject("Something went wrong");
+  }
+});
+
+promise
+  .then((result) => {
+    console.log("Promise Resolve and result: ", result);
+  })
+  .catch((err) => {
+    console.log("Something went wrong: ", err);
+  })
+  .finally(() => {
+    console.log("All Done");
+  });
+
+console.log(promise);
