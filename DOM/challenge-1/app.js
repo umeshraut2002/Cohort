9aa330380e1f642b8407b7043e23aa1721a3a23d
umesh raut <uraut6044@gmail.com>
2025-03-12 19:35:30 +0530

    challenge 02 updated

--- DOM/challenge-1/app.js
+++ DOM/challenge-1/app.js
@@ -5,6 +5,7 @@
 const bulb = document.getElementById("bulb");
 const button = document.getElementById("toggleButton");
 const statusText = document.getElementById("status");
+const body = document.querySelector("body");
 
 button.addEventListener("click", () => {
     if(bulb.classList.contains("off")){
@@ -12,11 +13,15 @@ button.addEventListener("click", () => {
         bulb.classList.add("ON");
         button.innerText = "Turn Off";
         statusText.innerText = "Status: ON";
+        body.style.backgroundColor = "black";
+        body.style.color = "yellow";
     }
     else{
         bulb.classList.remove("ON");
         bulb.classList.add("off");
         button.innerText = "Turn On";
         statusText.innerText = "Status: Off";
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
     }
 })
