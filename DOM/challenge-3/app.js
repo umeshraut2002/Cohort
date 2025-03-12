c431df5c0a112b4d16742a69309387cba55629f5
umesh raut <uraut6044@gmail.com>
2025-03-12 23:23:09 +0530

    challenge 03 completed

--- DOM/challenge-3/app.js
+++ DOM/challenge-3/app.js
@@ -1,3 +1,41 @@
 /**
  * Write your challenge solution here
  */
+
+const formContainer = document.querySelector("form-container");
+const nameInput = document.getElementById("nameInput");
+const jobInput = document.getElementById("jobInput");
+const ageInput = document.getElementById("ageInput");
+const bioInput = document.getElementById("bioInput");
+
+
+// for the display of live preview 
+const nameDisplay = document.getElementById("nameDisplay");
+const jobDisplay = document.getElementById("jobDisplay");
+const ageDisplay = document.getElementById("ageDisplay");
+const bioDisplay = document.getElementById("bioDisplay");
+
+// for display in ral time to name
+function realTimeNameInput(){
+    nameDisplay.innerText = nameInput.value;
+}
+
+function realTimeJobInput(){
+    jobDisplay.innerText = jobInput.value;
+}
+
+function realTimeAgeInput(){
+    ageDisplay.innerText = ageInput.value;
+}
+
+function realTimeBioInput(){
+    bioDisplay.innerText = bioInput.value;
+}
+
+nameInput.addEventListener("input", realTimeNameInput);
+
+jobInput.addEventListener("input", realTimeJobInput);
+
+ageInput.addEventListener("input", realTimeAgeInput);
+
+bioInput.addEventListener("input",realTimeBioInput);
