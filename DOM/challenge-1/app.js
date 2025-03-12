d862022572de28cd8e0b93703d96ed3588bc3c95
umesh raut <uraut6044@gmail.com>
2025-03-12 19:30:45 +0530

    challenge 01 completed

--- DOM/challenge-1/app.js
+++ DOM/challenge-1/app.js
@@ -2,10 +2,21 @@
  * Write your challenge solution here
  */
 
-const togleBtn = document.getElementById("toggleButton");
-// let bulb = document.getElementByClassName("bulb off");
+const bulb = document.getElementById("bulb");
+const button = document.getElementById("toggleButton");
+const statusText = document.getElementById("status");
 
-togleBtn.addEventListener("click", () => {
-    let bulb = document.getElementByClassName("bulb off");
-    bulb.style.backgroundColor = "yellow";
+button.addEventListener("click", () => {
+    if(bulb.classList.contains("off")){
+        bulb.classList.remove("off");
+        bulb.classList.add("ON");
+        button.innerText = "Turn Off";
+        statusText.innerText = "Status: ON";
+    }
+    else{
+        bulb.classList.remove("ON");
+        bulb.classList.add("off");
+        button.innerText = "Turn On";
+        statusText.innerText = "Status: Off";
+    }
 })
