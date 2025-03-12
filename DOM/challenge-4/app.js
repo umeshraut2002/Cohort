96be58be3c6b0060462f6ecd8f69c5ff8125f2e5
umesh raut <uraut6044@gmail.com>
2025-03-13 00:08:08 +0530

    challenge 04

--- DOM/challenge-4/app.js
+++ DOM/challenge-4/app.js
@@ -1,3 +1,20 @@
 /**
  * Write your challenge solution here
  */
+
+
+const taskInput = document.getElementById("taskInput");
+const addButton = document.getElementById("addButton");
+
+// add task into task list 
+const taskList = document.getElementById("taskList");
+
+// total task 
+const totalTasks = document.getElementById("totalTask");
+
+// complted tasks
+const completedTasks = document.getElementById("completeedTasks");
+
+addButton.addEventListener("click", () =>{
+    
+})
