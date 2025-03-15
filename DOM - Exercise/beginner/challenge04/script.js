9544d8e4875e9c416404569fbd069ddedf6f344d
umesh raut <uraut6044@gmail.com>
2025-03-15 14:25:08 +0530

    challenge 04 completed

--- DOM - Exercise/beginner/challenge04/script.js
+++ DOM - Exercise/beginner/challenge04/script.js
@@ -2,3 +2,11 @@ const decrement = document.getElementById("decrement");
 const increment = document.getElementById("increment");
 const count = document.getElementById("count");
 let c = 0;
+
+increment.addEventListener("click", () => {
+    count.innerText = c++;
+});
+
+decrement.addEventListener("click", ()=>{
+    count.innerText = c--;
+});
