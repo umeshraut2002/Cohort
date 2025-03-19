e3d5585666d5f9e2295c1d207d77d1fc7dc07cba
umesh raut <uraut6044@gmail.com>
2025-03-19 19:59:06 +0530

    drag and drop

---
+++ DOM - Exercise/intermediate/challenge02/script.js
@@ -0,0 +1,6 @@
+const dragContainer = document.getElementById("drag-container");
+const dragBox = document.getElementById("drag-box");
+
+dragBox.style.backgroundColor = "yellow";
+
+dragContainer.style.backgroundColor = "black";
