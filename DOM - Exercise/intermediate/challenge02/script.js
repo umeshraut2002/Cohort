bba3f5e36f002c2a9a2c77e8d1b2d1edee899318
umesh raut <uraut6044@gmail.com>
2025-03-23 00:52:48 +0530

    challenge of drag and drop done

--- DOM - Exercise/intermediate/challenge02/script.js
+++ DOM - Exercise/intermediate/challenge02/script.js
@@ -4,3 +4,34 @@ const dragBox = document.getElementById("drag-box");
 dragBox.style.backgroundColor = "yellow";
 
 dragContainer.style.backgroundColor = "black";
+
+let newX = 0;
+let newY = 0;
+let startX = 0;
+let startY = 0;
+
+function mouseDown(e){
+    startX = e.clientX;
+    startY = e.clientY;
+
+    document.addEventListener('mousemove', mouseMove);
+    document.addEventListener('mouseup', mouseUp);
+}
+
+function mouseMove(e){
+    newX = startX - e.clientX;
+    newY = startY - e.clientY;
+
+    startX = e.clientX;
+    startY = e.clientY;
+
+    dragBox.style.top = (dragBox.offsetTop - newY) + 'px';
+    dragBox.style.left = (dragBox.offsetLeft - newX) + 'px'; 
+}
+
+function mouseUp(){
+    document.removeEventListener('mousemove', mouseMove);
+}
+
+
+dragBox.addEventListener('mousedown', mouseDown);
