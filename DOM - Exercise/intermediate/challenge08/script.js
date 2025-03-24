a4653e848e0fe3068d82934a4a779fe5dd986690
umesh raut <uraut6044@gmail.com>
2025-03-24 14:40:03 +0530

    challenge08 completed

---
+++ DOM - Exercise/intermediate/challenge08/script.js
@@ -0,0 +1,3 @@
+const changeBtn = document.getElementById('change-btn').addEventListener('click', () => {
+    document.getElementById('myImage').src = 'https://images.pexels.com/photos/40896/larch-conifer-cone-branch-tree-40896.jpeg?auto=compress&cs=tinysrgb&w=600';
+})
